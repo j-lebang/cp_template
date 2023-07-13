66cf40d7673035978fda2678f9f3aa07b9594a8a
Susilo Lebang <138869995+j-lebang@users.noreply.github.com>
2023-07-14 09:52:42 +1000

    Create template.cpp

---
+++ template.cpp
@@ -0,0 +1,29 @@
+#include "bits/stdc++.h"
+using namespace std;
+
+using lint = long long;
+using ii = pair<int, int>;
+
+#define fi first
+#define se second
+
+// const int N = 2e5 + 5;
+const int MOD = 1e9 + 7;
+const int dr[] = {-1, 0, 0, 1};
+const int dc[] = {0, -1, 1, 0};
+
+void solve() {
+    
+}
+
+int main() {
+    ios::sync_with_stdio(0);
+    cin.tie(0); cout.tie(0);
+
+    int T = 1;
+    // cin >> T;
+    for (int tc = 0; tc < T; tc++) {
+        // cout << "Case " << tc << ": ";
+        solve();
+    }
+}
